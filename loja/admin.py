e0bbf22cd2e6db2a849ba391f1582e68c3367c0a
Luiz Gustavo <luizgustavo12125@email.com>
2026-06-04 15:33:17 -0300

    troca da estrutura do admin

--- loja/admin.py
+++ loja/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from django.contrib import admin
-
 from .models import (
     Categoria,
     Produto,
@@ -10,8 +8,79 @@ from .models import (
     ItemPedido
 )
 
-admin.site.register(Categoria)
-admin.site.register(Produto)
-admin.site.register(Cliente)
-admin.site.register(Pedido)
-admin.site.register(ItemPedido)
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'nome'
+    )
+
+    search_fields = (
+        'nome',
+    )
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'nome',
+        'categoria',
+        'preco',
+        'estoque',
+        'ativo'
+    )
+
+    search_fields = (
+        'nome',
+    )
+
+    list_filter = (
+        'categoria',
+        'ativo'
+    )
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'usuario',
+        'telefone'
+    )
+
+    search_fields = (
+        'usuario__username',
+    )
+
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'cliente',
+        'status',
+        'total',
+        'criado_em'
+    )
+
+    list_filter = (
+        'status',
+    )
+
+
+@admin.register(ItemPedido)
+class ItemPedidoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'pedido',
+        'produto',
+        'quantidade',
+        'preco'
+    )

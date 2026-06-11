3d2e2bb165d13e0424b79bad9f872a30d066f401
Luiz Gustavo <luizgustavo12125@email.com>
2026-06-11 20:16:13 -0300

    adicionando build.sh

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -121,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Buscadores essenciais para mapear e carregar a pasta static da sua 'loja' automaticamente
 STATICFILES_FINDERS = [
